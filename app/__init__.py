6e9941aefcd915997318353790c9dee4f6f56139
diggty66 <diggty66@gmail.com>
2025-10-29 12:39:49 -0400

    Load configuration from .env

--- app/__init__.py
+++ app/__init__.py
@@ -5,24 +5,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
+from dotenv import load_dotenv        # NEW: load .env variables
 import os
+
+# --- Load environment variables from .env ---
+load_dotenv()
 
 app = Flask(__name__)
 
 # --- Core configuration ---
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Mowed2.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///Mowed2.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'devsecret')
-app.config['SECURITY_PASSWORD_SALT'] = os.environ.get('SECURITY_PASSWORD_SALT', 'devsalt')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'devsecret')
+app.config['SECURITY_PASSWORD_SALT'] = os.getenv('SECURITY_PASSWORD_SALT', 'devsalt')
 
 # --- Mail configuration ---
 app.config.update(
-    MAIL_SERVER=os.environ.get('MAIL_SERVER', 'smtp.gmail.com'),
-    MAIL_PORT=int(os.environ.get('MAIL_PORT', 587)),
-    MAIL_USE_TLS=True,
-    MAIL_USERNAME=os.environ.get('MAIL_USERNAME', ''),
-    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD', ''),
-    MAIL_DEFAULT_SENDER=os.environ.get('MAIL_DEFAULT_SENDER', '')
+    MAIL_SERVER=os.getenv('MAIL_SERVER', 'smtp.gmail.com'),
+    MAIL_PORT=int(os.getenv('MAIL_PORT', 587)),
+    MAIL_USE_TLS=os.getenv('MAIL_USE_TLS', 'True') == 'True',
+    MAIL_USERNAME=os.getenv('MAIL_USERNAME', ''),
+    MAIL_PASSWORD=os.getenv('MAIL_PASSWORD', ''),
+    MAIL_DEFAULT_SENDER=os.getenv('MAIL_DEFAULT_SENDER', '')
 )
 
 mail = Mail(app)
