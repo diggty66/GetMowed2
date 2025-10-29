130af0eda9cb8a6ebcf68089a2fec75feabf4d35
diggty66 <diggty66@gmail.com>
2025-10-29 12:08:16 -0400

    Simplify config and fix Flask config.load

--- app/__init__.py
+++ app/__init__.py
@@ -1,62 +1,47 @@
 """
 The flask application package.
 """
-from flask import Flask, Blueprint
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_mail import Mail, Message
-import json
- 
+from flask_mail import Mail
+import os
+
 app = Flask(__name__)
-app.config.from_object(__name__)
-# from GetMowed2 import app as application
 
-#with open('/etc/config.json') as config_file:
-#  config = json.load(config_file)
-
-app.config.from_file("/etc/config.json", load=json.load)
-
-# Magic that creates everything, including Base (db.Model), engine (db.engine), and session >
+# --- Core configuration ---
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Mowed2.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'SQLALCHEMY_TRACK_MODIFICATIONS'
-app.config['SECRET_KEY'] = 'SECRET_KEY'
-app.config['SECURITY_PASSWORD_SALT'] = 'SECURITY_PASSWORD_SALT'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'devsecret')
+app.config['SECURITY_PASSWORD_SALT'] = os.environ.get('SECURITY_PASSWORD_SALT', 'devsalt')
 
-# mail settings
-app.config['MAIL_SERVER']= 'MAIL_SERVER'
-app.config['MAIL_PORT'] = 'MAIL_PORT'
-app.config['MAIL_USE_TLS'] = 'MAIL_USE_TLS'
-app.config['MAIL_USE_SSL'] = 'MAIL_USE_SSL'
-app.config['TESTING'] = 'TESTING'
-app.config['MAIL_SUPPRESS_SEND'] = 'MAIL_SUPPRESS_SEND'
-app.config['MAIL_FAIL_SILENTLY'] = 'MAIL_FAIL_SILENTLY' 
+# --- Mail configuration ---
+app.config.update(
+    MAIL_SERVER=os.environ.get('MAIL_SERVER', 'smtp.gmail.com'),
+    MAIL_PORT=int(os.environ.get('MAIL_PORT', 587)),
+    MAIL_USE_TLS=True,
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME', ''),
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD', ''),
+    MAIL_DEFAULT_SENDER=os.environ.get('MAIL_DEFAULT_SENDER', '')
+)
 
-# gmail authentication
-app.config['MAIL_USERNAME'] = 'MAIL_USERNAME'
-app.config['MAIL_PASSWORD'] = 'MAIL_PASSWORD'
-
-# mail accounts
-app.config['MAIL_DEFAULT_SENDER'] = 'MAIL_DEFAULT_SENDER'
 mail = Mail(app)
-
 db = SQLAlchemy(app)
 
-# flask-user
+# --- Flask-Login setup ---
 login_manager = LoginManager(app)
-login_manager.init_app(app)
 login_manager.login_view = 'login'
-login_manager.login_view = 'admin'
 
-# import a blueprints
-from GetMowed2.articles.views import articles_blueprint
-from GetMowed2.dashboards.views import dashboards_blueprint
-from GetMowed2.emails.views import emails_blueprint
-from GetMowed2.home.views import home_blueprint
-from GetMowed2.profiles.views import profiles_blueprint
-from GetMowed2.users.views import users_blueprint
-from GetMowed2.admin.views import admin_blueprint
+# --- Import blueprints ---
+from app.articles.views import articles_blueprint
+from app.dashboards.views import dashboards_blueprint
+from app.emails.views import emails_blueprint
+from app.home.views import home_blueprint
+from app.profiles.views import profiles_blueprint
+from app.users.views import users_blueprint
+from app.admin.views import admin_blueprint
 
-# register blueprints with the application
+# --- Register blueprints ---
 app.register_blueprint(articles_blueprint, url_prefix='/articles')
 app.register_blueprint(dashboards_blueprint, url_prefix='/dashboards')
 app.register_blueprint(emails_blueprint, url_prefix='/emails')
@@ -65,5 +50,6 @@ app.register_blueprint(profiles_blueprint, url_prefix='/profiles')
 app.register_blueprint(users_blueprint, url_prefix='/users')
 app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
-# create all tables
-models.db.create_all()
+# --- Create DB tables if needed ---
+with app.app_context():
+    db.create_all()
